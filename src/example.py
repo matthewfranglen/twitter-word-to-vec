ac68698e813b1f70dc0a7cacb3f77e726d78beec
Matthew Franglen <matthew@franglen.org>
2017-07-18 21:32:53 +0100

    Initial commit of example code

---
+++ src/example.py
@@ -0,0 +1,19 @@
+import pandas as pd # provide sql-like data manipulation tools. very handy.
+pd.options.mode.chained_assignment = None
+import numpy as np # high dimensional vector computing library.
+from copy import deepcopy
+from string import punctuation
+from random import shuffle
+
+import gensim
+from gensim.models.word2vec import Word2Vec # the word2vec model gensim class
+LabeledSentence = gensim.models.doc2vec.LabeledSentence # we'll talk about this down below
+
+from tqdm import tqdm
+tqdm.pandas(desc="progress-bar")
+
+from nltk.tokenize import TweetTokenizer # a tweet tokenizer from nltk.
+tokenizer = TweetTokenizer()
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
